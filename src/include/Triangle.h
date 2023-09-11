66e27ac543ca89e65845a5758f0746bada01c3e5
mdcra <maria.ramos21@upr.edu>
2023-09-11 17:30:22 -0400

    test add comments

--- src/include/Triangle.h
+++ src/include/Triangle.h
@@ -2,26 +2,31 @@
 class Triangle {
 
   private:
+  // members
   int sides[3];
   // int s1;
   // int s2;
   // int s3;
 
   public:
+  // getters
   int getSide1() { return sides[0]; }
   int getSide2() { return sides[1]; }
   int getSide3() { return sides[2]; }
-
+  
+  // setters
   void setSide1(int s) { sides[0] = s; }
   void setSide2(int s) { sides[1] = s; }
   void setSide3(int s) { sides[2] = s; }
 
+  // constructor
   Triangle(int a, int  b, int c) {
     sides[0] = a;
     sides[1] = b;
     sides[2] = c;
   }
 
+  // methods
   double getArea() {
     double s = (this->getSide1() + this->getSide2() + this->getSide3()) / 2.0;
     double area = sqrt((s * (s - this->getSide1()) * (s - this->getSide2()) * (s - this->getSide3())));
